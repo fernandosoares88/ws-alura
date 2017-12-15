94700b4090273a21e35d4866d52421c82956d0b7
fernando <fernando@note-hp>
2017-12-15 10:49:56 -0300

    Alterando logout para utilizar interface tarefa.

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -1,30 +1,20 @@
 package br.com.alura.gerenciador.web;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet(urlPatterns = "/logout")
-public class Logout extends HttpServlet {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 7763927224856106937L;
+public class Logout implements Tarefa {
+
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+	public String executa(HttpServletRequest req, HttpServletResponse resp) {
 		HttpSession session = req.getSession();
 		session.removeAttribute("usuarioLogado");
 
-		req.getRequestDispatcher("/WEB-INF/paginas/logout.html").forward(req, resp);
-
+		return "/WEB-INF/paginas/logout.html";
+		
 	}
 
 }
